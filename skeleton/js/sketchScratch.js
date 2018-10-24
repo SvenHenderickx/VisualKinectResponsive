42bc481c85a28e619bfa6fb8b1bdc9f883ec423e
Sven <henderickx.sven@gmail.com>
2018-10-24 11:31:50 +0200

    hand tracking multiple people

--- skeleton/js/sketchScratch.js
+++ skeleton/js/sketchScratch.js
@@ -7,22 +7,6 @@ let kinectronIpAddress = "145.93.182.29";
 // declare kinectron
 let kinectron = null;
 
-// drawHand variables
-let start = 30;
-let target = 100;
-let diameter = start;
-let light = 255;
-let dark = 100;
-let hueValue = light;
-let lerpAmt = 0.3;
-let state = 'ascending';
-
-// recorded data variables
-let sentTime = Date.now();
-let currentFrame = 0;
-let recorded_skeleton;
-let recorded_data_file = "./js/recorded_skeleton.json";
-
 var xHead;
 var yHead;
 
@@ -31,18 +15,19 @@ var yRight;
 var xLeft;
 var yLeft;
 
+var handsClose = [];
 
 function setup() {
   createCanvas(1920,1080);
 
-  kinectron = new Kinectron("145.93.182.29");
+  kinectron = new Kinectron("145.93.44.6");
 
   kinectron.makeConnection();
 
   kinectron.startTrackedBodies(drawBody);
 
   // createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 300);
+  colorMode(HSB, 100);
 }
 
 function drawBody(body){
@@ -59,8 +44,6 @@ function drawBody(body){
     }
 
     if(i == 11 || i == 7){
-      // fill(255,0,0);
-      // ellipse(body.joints[i].depthX * width, body.joints[i].depthY * height, 20, 20);
       if(i == 11){
         xRight = body.joints[i].depthX * width;
         yRight = body.joints[i].depthY * height;
@@ -70,23 +53,23 @@ function drawBody(body){
         yLeft = body.joints[i].depthY * height;
       }
     }
-
-    checkHandDistance();
   }
+
+  checkHandDistance(body);
 }
 
 // Rain ~!!!!
 
 var allParticles = [];
 var globalHue = 120;
-var spawnPerFrame = 8;
+var spawnPerFrame = 5;
 var mouseSize = 120;
 
 
 function Particle(x, y) {
   this.lastPos = new p5.Vector(x, y);
   this.pos = new p5.Vector(x, y);
-  this.vel = new p5.Vector(0, 0);
+  this.vel = new p5.Vector(0, 7);
   this.acc = new p5.Vector(0, 0);
   this.size = random(2, 20);
   this.h = globalHue;
@@ -143,14 +126,35 @@ function draw() {
   }
 }
 
-function checkHandDistance(){
+function checkHandDistance(body){
+  var bodyDouble = false;
   var distance = dist(xRight, yRight, xLeft, yLeft);
-  console.log(distance);
+  // console.log(body);
+  // console.log(distance);
   if(distance < 300){
-    // fill(0,0,255);
-    // ellipse(width / 2, height / 2, 50, 50);
+    for(var i = 0; i < handsClose.length; i++){
+      if(handsClose[i] == body.trackingId){
+        bodyDouble = true;
+      }
+    }
+
+    if(bodyDouble == false){
+      handsClose.push(body.trackingId);
+      console.log("added body");
+    }
+  }
+  else{
+    var index = handsClose.indexOf(body.trackingId);
+    if(index > -1){
+      handsClose.splice(index, 1);
+      console.log("removed body");
+    }
+  }
+
+  if(handsClose.length > 1){
     spawnPerFrame = 1;
-  }else{
-    spawnPerFrame = 8;
+  }
+  else{
+    spawnPerFrame = 5;
   }
 }

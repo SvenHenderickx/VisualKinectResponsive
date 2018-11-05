c1a558311f4f08f68f11a7cbb75be5c3f73c7401
Sven <henderickx.sven@gmail.com>
2018-11-05 10:51:12 +0100

    all joints

--- Gevroren/frozen.js
+++ Gevroren/frozen.js
@@ -47,19 +47,19 @@ var data = [];
 function Particle(x, y, level) {
   this.level = level;
   this.life = 0;
-  
+
   this.pos = new p5.Vector(x, y);
   this.vel = p5.Vector.random2D();
   this.vel.mult(map(this.level, 0, maxLevel, 5, 2));
-  
+
   this.move = function() {
     this.life++;
-    
+
     // Add friction.
     this.vel.mult(0.9);
-    
+
     this.pos.add(this.vel);
-    
+
     // Spawn a new particle if conditions are met.
     if (this.life % 10 == 0) {
       if (this.level > 0) {
@@ -75,23 +75,23 @@ function Particle(x, y, level) {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-    kinectron = new Kinectron("145.93.82.36");
+    kinectron = new Kinectron("145.93.81.21");
 
     kinectron.makeConnection();
 
     kinectron.startTrackedBodies(drawBody);
-  
+
   colorMode(HSB, 360);
-  
+
   textAlign(CENTER);
-  
+
   background(0);
 }
 
 function drawBody(body){
     // background(0);
     for(var i = 0; i < body.joints.length; i++){
-
+        checkHand(body.joints[i].depthX * width, body.joints[i].depthY * height);
         if(i == 3){
             // fill(255,0,0);
             // ellipse(body.joints[i].depthX * width, body.joints[i].depthY * height, 20, 20);
@@ -113,56 +113,58 @@ function drawBody(body){
         }
     }
 
-    checkHand();
+
     //checkHandDistance();
 }
+
+
 
 function draw() {
   // Create fade effect.
   noStroke();
   fill(0, 30);
   rect(0, 0, width, height);
-  
+
   // Move and spawn particles.
   // Remove any that is below the velocity threshold.
   for (var i = allParticles.length-1; i > -1; i--) {
     allParticles[i].move();
-    
-    if (allParticles[i].vel.mag() < 0.5) {
+
+    if (allParticles[i].vel.mag() < .7) {
       allParticles.splice(i, 1);
     }
   }
-  
+
   if (allParticles.length > 0) {
     // Run script to get points to create triangles with.
     data = Delaunay.triangulate(allParticles.map(function(pt) {
       return [pt.pos.x, pt.pos.y];
     }));
-    
+
     strokeWeight(0.1);
-    
+
     // Display triangles individually.
     for (var i = 0; i < data.length; i += 3) {
       // Collect particles that make this triangle.
       var p1 = allParticles[data[i]];
       var p2 = allParticles[data[i+1]];
       var p3 = allParticles[data[i+2]];
-      
+
       // Don't draw triangle if its area is too big.
       var distThresh = 500;
-      
+
       if (dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y) > distThresh) {
         continue;
       }
-      
+
       if (dist(p2.pos.x, p2.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
         continue;
       }
-      
+
       if (dist(p1.pos.x, p1.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
         continue;
       }
-      
+
       // Base its hue by the particle's life.
       if (useFill) {
         noStroke();
@@ -171,21 +173,21 @@ function draw() {
         noFill();
         stroke(165+p1.life*1.5, 360, 360);
       }
-      
-      triangle(p1.pos.x, p1.pos.y, 
-               p2.pos.x, p2.pos.y, 
+
+      triangle(p1.pos.x, p1.pos.y,
+               p2.pos.x, p2.pos.y,
                p3.pos.x, p3.pos.y);
     }
   }
-  
+
   noStroke();
   fill(255);
   //text("Click and drag the mouse\nPress any key to change to fill/stroke", width/2, height-50);
 }
 
 
-function checkHand() {
-  allParticles.push(new Particle(xHead, yHead, maxLevel));
+function checkHand(xPos, yPos) {
+  allParticles.push(new Particle(xPos, yPos, maxLevel));
 }
 
 // function checkHandDistance(){
